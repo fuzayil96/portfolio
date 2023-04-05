c7d4892778d10dfe8fba96175662247eaa11dbb6
fuzayil96 <85925862+fuzayil96@users.noreply.github.com>
2023-04-05 16:15:20 +0500

    3 files modified

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 const scroll = new SmoothScroll('.menu a[href*="#"]', {
   speed: 700,
 });
+
+const menuButton = document.querySelector('.mobil-menu')
+const hiddenMenu = document.querySelector('.menu-hidden')
+
+menuButton.addEventListener('click', ()=>{
+hiddenMenu.classList.toggle('active')
+})
